e0bf253c5620ff78bc8d26925d44f81fcfc49123
Muhammad Asif <asifsaab098@gmail.com>
2023-12-01 06:40:57 -0800

    after production

--- app/Navbar/contactus/page.jsx
+++ app/Navbar/contactus/page.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ContactPage = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100 ">
+    <div className="flex justify-center items-center min-h-screen bg-gray-100 text-black">
       <div className="w-full max-w-md p-6 bg-white rounded shadow-md">
         <h1 className="text-3xl font-semibold mb-6 text-center">Contact Us</h1>
         <form action="https://formspree.io/f/mleyyone" method='POST' className="space-y-4">
