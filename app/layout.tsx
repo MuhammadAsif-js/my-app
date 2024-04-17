0ccfea5653382760c6a69a54b90c53af23a6c60e
Muhammad Asif <asifsaab098@gmail.com>
2024-04-17 15:46:02 -0700

    head update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./Navbar/page"; // Adjust the path as needed
+import Navbary from "./Navbar/page.jsx";
 import Footer from "../app/Tools.logic/footer/page";
 import "./globals.css";
 import { Metadata } from "next";
@@ -11,16 +11,42 @@ export const metadata: Metadata = {
     template: "%s | Our calculators",
   },
   description:
-    "Our calculators provide wide range of calculators to perform your day to day tasks efficiently",
+    "Our calculators provide wide range of calculators to perform your day to day tasks effeciently",
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
   <html lang="en">
     <head>
-      {/* Your head content here */}
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+        name="google-site-verification"
+        content="dGBBkNwH0r3qsHb2D8y1KYPIFYDC6SFq8k4x72hpqJw"
+      />
+      
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap"
+      />
+
+      <Script
+        strategy="afterInteractive" //lazyOnload
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.CALCULATORS_TRACKING_ID}`}
+      />
+
+      <Script id="google-analytics-script" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.CALCULATORS_TRACKING_ID}', {
+          page_path: window.location.pathname,
+          });
+    `}
+      </Script>
     </head>
     <body>
-      <Navbar /> {/* Include Navbar component here */}
+      <Navbary />
       {children}
       <Footer />
     </body>
@@ -28,64 +54,6 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => (
 );
 
 export default RootLayout;
-
-
-// import React from "react";
-// import Navbary from "./Navbar/page.jsx";
-// import Footer from "../app/Tools.logic/footer/page";
-// import "./globals.css";
-// import { Metadata } from "next";
-// import Script from "next/script";
-
-// export const metadata: Metadata = {
-//   title: {
-//     default: "Our calculators",
-//     template: "%s | Our calculators",
-//   },
-//   description:
-//     "Our calculators provide wide range of calculators to perform your day to day tasks effeciently",
-// };
-
-// const RootLayout = ({ children }: { children: React.ReactNode }) => (
-//   <html lang="en">
-//     <head>
-//       <meta name="viewport" content="width=device-width, initial-scale=1" />
-//       <meta
-//         name="google-site-verification"
-//         content="dGBBkNwH0r3qsHb2D8y1KYPIFYDC6SFq8k4x72hpqJw"
-//       />
-      
-//       <link rel="preconnect" href="https://fonts.googleapis.com" />
-//       <link
-//         rel="stylesheet"
-//         href="https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap"
-//       />
-
-//       <Script
-//         strategy="afterInteractive" //lazyOnload
-//         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.CALCULATORS_TRACKING_ID}`}
-//       />
-
-//       <Script id="google-analytics-script" strategy="lazyOnload">
-//         {`
-//           window.dataLayer = window.dataLayer || [];
-//           function gtag(){dataLayer.push(arguments);}
-//           gtag('js', new Date());
-//           gtag('config', '${process.env.CALCULATORS_TRACKING_ID}', {
-//           page_path: window.location.pathname,
-//           });
-//     `}
-//       </Script>
-//     </head>
-//     <body>
-//       <Navbary />
-//       {children}
-//       <Footer />
-//     </body>
-//   </html>
-// );
-
-// export default RootLayout;
 
 // import React from "react";
 // import Navbar from "./Navbar/page";
